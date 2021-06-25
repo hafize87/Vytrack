f64f7fd5f50e23096cda00f7c2e066d871957d68
Hafize Aksoy <hafize.teacher@gmail.com>
2021-06-24 20:50:34 -0700

    AC1_2

--- src/test/java/com/vytrack/tests/US3_Buttons/AC_1_2_3_4_5_6_7_8.java
+++ src/test/java/com/vytrack/tests/US3_Buttons/AC_1_2_3_4_5_6_7_8.java
@@ -11,7 +11,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class AC_1_3_4_5 {
+public class AC_1_2_3_4_5_6_7_8 {
 
     @BeforeClass
     public void setUp()  {
@@ -36,6 +36,23 @@ public class AC_1_3_4_5 {
         WebElement csvXlsxOption = Driver.getDriver().findElement(By.xpath("(//ul[@class = 'dropdown-menu'])[3]"));
 
         Assert.assertTrue(csvXlsxOption.isDisplayed(), "csvXlsxOption is not displayed. FAIL!!!");
+    }
+
+    @Test
+    public void tc002_Dropdown_Is_On_Left() {
+
+        BrowserUtils.sleep(2);
+        WebElement exportGrit = Driver.getDriver().findElement(By.xpath("//a[@title='With this button you will export the content of the grid as it appears to you, with filters and sorting applied. All pages will be exported.']/../../../../preceding-sibling::div"));
+
+        String export = exportGrit.getAttribute("class");
+
+        if(export.contains("left")){
+            System.out.println("Export Grid dropdown is on the left of the page");
+        }
+        else{
+            System.out.println("Export Grid dropdown is not on the left of the page");
+        }
+
     }
 
 
@@ -110,11 +127,57 @@ public class AC_1_3_4_5 {
 
     }
 
+
+    @Test
+    public void tc006_Reset_Refresh_Button() {
+
+        BrowserUtils.sleep(2);
+        WebElement refresh = Driver.getDriver().findElement(By.xpath("//a[@title = 'Refresh']//following-sibling::a[@title='Reset']"));
+
+        Assert.assertTrue(refresh.isDisplayed());
+
+
+    }
+
+
+
+    @Test
+    public void tc007_Grid_Settings_on_The_Right_of_The_Reset() {
+
+
+
+        BrowserUtils.sleep(2);
+        WebElement gridSettingRight = Driver.getDriver().findElement(By.xpath("//a[@title='Grid Settings']/../../a[3]"));
+
+        Assert.assertTrue(gridSettingRight.isDisplayed());
+
+
+    }
+
+
+    @Test
+    public void tc008_Grid_Settings_on_The_Righ_of_The_Page() {
+
+
+        BrowserUtils.sleep(2);
+        WebElement exportGrit = Driver.getDriver().findElement(By.xpath("//a[@title='Grid Settings']/../../.."));
+
+        String export = exportGrit.getAttribute("class");
+
+        if(export.contains("right")){
+            System.out.println("Export Grid dropdown is on the right of the page");
+        }
+        else{
+            System.out.println("Export Grid dropdown is not on the right of the page");
+        }
+
+
+    }
+
+
     @AfterClass
     public void tearDown() {
         Driver.closeDriver();
     }
-
-
 
 }
